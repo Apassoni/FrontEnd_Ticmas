368b93d6aeed08a7f99d39eb5992d33af27dad0c
Atilio Passoni <atiliopassoni@gmail.com>
2023-02-28 20:19:17 -0300

    Agregado de funciones JS

---
+++ FRONTEND/js/javascript.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = "orange"; 
+function cambiarTema(){
+    if (document.body.style.backgroundColor == "orange"){
+        document.body.style.backgroundColor = "lightskyblue";
+    } else {
+        document.body.style.backgroundColor = "orange";
+        
+    }
+}
+
