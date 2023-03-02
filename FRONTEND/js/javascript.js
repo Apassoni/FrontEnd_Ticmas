7d15a199abc97d69ac192f7dd3961624c39c520e
Atilio Passoni <atiliopassoni@gmail.com>
2023-03-02 16:33:06 -0300

    Mejora visual - Postergacion fecha de entrega

--- FRONTEND/js/javascript.js
+++ FRONTEND/js/javascript.js
@@ -1,9 +1,13 @@
 document.body.style.backgroundColor = "orange"; 
+document.getElementById("barra_navegacion").style.backgroundColor = "orange";
 function cambiarTema(){
     if (document.body.style.backgroundColor == "orange"){
         document.body.style.backgroundColor = "lightskyblue";
+        document.getElementById("barra_navegacion").style.backgroundColor = "lightskyblue";
+        
     } else {
         document.body.style.backgroundColor = "orange";
+        document.getElementById("barra_navegacion").style.backgroundColor = "orange";
         
     }
 }
